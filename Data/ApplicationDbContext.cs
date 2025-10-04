a0b7f35747d4c70d2f5b5f1f51ce5115478a85fc
kiaraLinares19 <kiaralinaresestivariz@gmail.com>
2025-10-04 15:24:54 -0500

    Dotnet

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ep_Linares.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
